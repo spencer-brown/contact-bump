1569d9f4a7b70377dc6379722ec60df609f6627d
Spencer Brown <me@spencer.sx>
2016-10-19 21:20:47 -0400

    Set up contact creation form

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+// Set up bodyParse.
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 // Set up templating.
 app.set('view engine', 'pug');
 
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,27 @@
+const sync = require('synchronize');
+const db = require('../db');
+
+
 function router(app) {
   app.get('/', (req, res, next) => {
-    const contacts = [{
-      name: 'Ben'
-    }, {
-      name: 'Rhys'
-    }, {
-      name: 'Evan'
-    }, {
-      name: 'Steezy'
-    }, {
-      name: 'Jess'
-    }];
+    sync.fiber(() => {
+      const contacts = sync.await(db.collection('contacts').find({}, sync.defer()));
 
-    res.render('index', {
-      contacts
+      res.render('index', {
+        contacts
+      });
+    });
+  });
+
+  app.post('/contacts', (req, res, next) => {
+    sync.fiber(() => {
+      const contact = {
+        name: req.body.name
+      };
+
+      sync.await(db.collection('contacts').insert(contact, sync.defer()));
+
+      res.redirect('/');
     });
   });
 }
