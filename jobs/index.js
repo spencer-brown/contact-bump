5139e228d0b341fce104276f14840027955a10fa
Spencer Brown <me@spencer.sx>
2016-10-19 23:06:09 -0400

    Get it going

--- jobs/index.js
+++ jobs/index.js
@@ -1,20 +1,64 @@
 const CronJob = require('cron').CronJob;
 const sync = require('synchronize');
+const twilio = require('twilio')('AC76f41e6e2fa773af3e74634d0fd2677b', '0c4f7b31b530bb6dc79a309801e7286d');
+const _ = require('underscore');
 
 const db = require('../db');
 
 
 const EVERY_SECOND_PATTERN = '* * * * * *';
+const THIRTY_SEC_IN_MS = 30 * 1000;
 
-// Check for contacts that are more than 1 minute old.
+// Check for contacts that are ready to bump.
 new CronJob(EVERY_SECOND_PATTERN, () => {
   sync.fiber(() => {
-    const contacts = sync.await(db.collection('contacts').find({
-      needsContacting: true
+    const needContacting = sync.await(db.collection('contacts').find({
+      // Expired contacts.
+      needsBumpAt: {
+        $lte: Date.now()
+      },
+
+      // Aren't in a pending state.
+      bumpedAt: null
     }, {
       name: 1
     }, sync.defer()));
 
-    console.log('needs contacting:', contacts);
+    if (_.isEmpty(needContacting)) {
+      console.log('no contacts to bump');
+      return;
+    } else {
+      console.log('need contacting', needContacting);
+    }
+
+    // Send text about contacts.
+    const joinedNames = _.pluck(needContacting, 'name').join(', ');
+    const body = `Needs contacting: ${joinedNames}`;
+    try {
+      sync.await(twilio.sendMessage({
+        body,
+        to: '+17404053797',
+        from: '+16697219661'
+      }, sync.defer()));
+    } catch(err) {
+      console.log('ERROR:', err);
+    }
+
+    // Set `bumpedAt` on contacts.
+    const contactIds = _.pluck(needContacting, '_id');
+    try {
+      sync.await(db.collection('contacts').update({
+        _id: {
+          $in: contactIds
+        }
+      }, {
+        $set: {
+          bumpedAt: Date.now()
+        }
+      }, sync.defer()));
+    } catch (err) {
+      console.log('ERROR:', err);
+    }
+    console.log('bumped', contactIds);
   });
 }, null, true, 'America/Los_Angeles');
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
+'use strict';
+
 const sync = require('synchronize');
+const mongojs = require('mongojs');
+
 const db = require('../db');
+
+
+const THIRTY_SEC_IN_MS = 30 * 1000;
 
 
 function router(app) {
@@ -7,8 +14,9 @@ function router(app) {
     sync.fiber(() => {
       const contacts = sync.await(db.collection('contacts').find({
       }, {
+        _id: 1,
         name: 1,
-        needsContacting: 1
+        bumpedAt: 1
       }, sync.defer()));
 
       res.render('index', {
@@ -19,13 +27,50 @@ function router(app) {
 
   app.post('/contacts', (req, res, next) => {
     sync.fiber(() => {
+      const needsBumpAt = (req.body.needsContacting) ? Date.now() : Date.now() + THIRTY_SEC_IN_MS;
       const contact = {
-        name: req.body.name,
-        needsContacting: !!req.body.needsContacting
+        needsBumpAt,
+        name: req.body.name
       };
-      console.log('req.body', req.body);
 
       sync.await(db.collection('contacts').insert(contact, sync.defer()));
+
+      res.redirect('/');
+    });
+  });
+
+  app.get('/testsms', (req, res, next) => {
+    sync.fiber(() => {
+      try {
+        sync.await(twilio.sendMessage({
+          to: '+17404053797',
+          from: '+16697219661',
+          body: 'test sms'
+        }, sync.defer()));
+      } catch (err) {
+        console.log('ERROR:', err);
+      }
+
+      res.send('Sent test text message.');
+    });
+  });
+
+  app.post('/clearBump', (req, res, next) => {
+    sync.fiber(() => {
+      const contactId = req.body.contactId;
+
+      try {
+        sync.await(db.collection('contacts').update({
+          _id: mongojs.ObjectId(contactId)
+        }, {
+          $set: {
+            bumpedAt: null,
+            needsBumpAt: Date.now() + THIRTY_SEC_IN_MS
+          }
+        }, sync.defer()));
+      } catch (err) {
+        console.log('ERROR:', err);
+      }
 
       res.redirect('/');
     });
