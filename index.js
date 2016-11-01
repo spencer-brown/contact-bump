f898f1181ab46d4b3011a4cc9cddddfe4a2ee5fa
Spencer Brown <me@spencer.sx>
2016-11-01 15:03:43 -0400

    Reorder app setup logic

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ app.use(express.static('public'));
 // Require routes.
 require('./routes')(app);
 
+// Set up environment variables.
+const args = process.argv.slice(2);
+// TODO: Figure out a better way to do this with grunt-nodemon.
+process.env.ENVIRONMENT = args[0].split('=')[1];
+
+// TODO: Make this dynamic based on environment.
+const config = require('./config');
+process.env.TWILIO_ACC_SID = config.TWILIO_ACC_SID;
+process.env.TWILIO_AUTH_TOKEN = config.TWILIO_AUTH_TOKEN;
+
 // Start the app.
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -24,12 +34,3 @@ app.listen(port, () => {
 
 // Start jobs.
 require('./jobs');
-
-// Set up environment variables.
-const args = process.argv.slice(2);
-// TODO: Figure out a better way to do this with grunt-nodemon.
-process.env.ENVIRONMENT = args[0].split('=')[1];
-
-const config = require('./config');
-process.env.TWILIO_ACC_SID = config.TWILIO_ACC_SID;
-process.env.TWILIO_AUTH_TOKEN = config.TWILIO_AUTH_TOKEN;
