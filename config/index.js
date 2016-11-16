ff6f5cc00deafb78b058d6029d213f2625eebe43
Spencer Brown <me@spencer.sx>
2016-11-15 23:52:45 -0500

    Log in config definition

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
  * Copy this file into config/index.js and complete with the appropriate values.
  */
 let config = {};
-if (process.env.ENVIRONMENT == 'prod') {
+if (process.env.ENVIRONMENT === 'prod') {
   config.TWILIO_ACC_SID = process.env.TWILIO_ACC_SID;
   config.TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
   config.REDIS_SECRET = process.env.REDIS_SECRET;
@@ -13,6 +13,7 @@ if (process.env.ENVIRONMENT == 'prod') {
   config.FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 } else {
   // dev
+  console.log('process.env.ENVIRONMENT was', process.env.ENVIRONMENT);
   config = require('./local');
 }
 
